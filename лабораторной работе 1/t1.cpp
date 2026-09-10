fcc16ead82c64515e9006755a062d840902b7af8
Gosha <goshashalnev@bk.ru>
2026-09-10 15:09:37 +0300

    Add files via upload

---
+++ лабораторной работе 1/t1.cpp
@@ -0,0 +1,22 @@
+﻿//Лабараторная работа 1
+// Вариант 14
+
+//Базовый уровень
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float hours = 0;
+    float minutes = 0;
+
+    cout << "Enter hours: ";
+
+    cin >> hours;
+
+    minutes = hours * 60;
+
+    cout << hours << " H = " << minutes << " M" << endl;
+    return 0;
+}
