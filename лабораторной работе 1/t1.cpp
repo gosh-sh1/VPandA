2d70048d7a811593b459894c622e7d11cb56bbcc
Gosha <goshashalnev@bk.ru>
2026-09-14 14:11:51 +0300

    Update t1.cpp

--- лабораторной работе 1/t1.cpp
+++ лабораторной работе 1/t1.cpp
@@ -8,22 +8,27 @@ using namespace std;
 
 int main()
 {
+   // Используем double, так как количество часов может быть дробным числом
     double hours = 0;
 
     cout << "Enter hours: ";
-    cin >> hours;
 
+    // Защита от некорректного ввода (буквы) и отрицательных значений
+    cin >> hours;
     while (cin.fail() || hours < 0)
     {
-        cin.clear();
+        cin.clear(); // Сброс флага ошибки cin
+        cin.ignore(10000, '\n'); // Очистка некорректных символов из буфера
 
-        cout << "Invalid input. Enter hours again: ";
+        cout << "Invalid input. Enter positive number of hours: ";
         cin >> hours;
     }
 
+    // Расчет: в одном часе 60 минут
     double minutes = hours * 60;
 
+    // Вывод результата с пояснениями
     cout << hours << " H = " << minutes << " M" << endl;
 
-    return 0;
+    return 0; // Успешный выход из программы
 }
