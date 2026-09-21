917d243ebb11808d53914b09cbf882679d084169
Gosha <goshashalnev@bk.ru>
2026-09-21 14:47:05 +0300

    Update t1.cpp

--- лабораторная работа 3/t1.cpp
+++ лабораторная работа 3/t1.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <clocale>
 
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int number = 0;
+
+    // Защита от некорректного ввода
+    cin >> number;
+    while (cin.fail())
+    {
+        cin.clear();
+        cin.ignore(10000, '\n');
+
+        cout << "Некорректный ввод. Введите число: ";
+        cin >> number;
+    }
+
+    //Проверка на кратность 15
+    if (number % 3 == 0 and number % 5 == 0)
+    {
+        cout << "Кратно 15" << endl;
+    }
+    else
+    {
+        cout << "Не кратно 15" << endl;
+    }
+
+    return 0;
+}
